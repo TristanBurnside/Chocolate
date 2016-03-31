041df353100e06e4536f2a6d0a70c438c0101624
Tristan Burnside <tristan.burnside@outware.com.au>
2016-04-01 09:54:06 +1100

    Add UITableViewController subclass modelled on the ChocolateCollectionViewController

---
+++ Chocolate/ChocolateTableViewContolller.swift
@@ -0,0 +1,164 @@
+//
+//  ChocolateTableViewContolller.swift
+//  Chocolate
+//
+//  Created by Tristan Burnside on 14/03/2016.
+//  Copyright © 2016 Tristan Burnside. All rights reserved.
+//
+
+import UIKit
+
+public class ChocolateTableViewContolller: UITableViewController, ConfigurableViewController {
+
+    //Note to self: this is AnyObject because protocol implementing properties cannot be @IBOutlet
+    @IBOutlet private var dataSource : AnyObject?
+    private var chocolateDataSource : ChocolateCellDataSource?
+    private let cellOperationQueue = NSOperationQueue()
+    private var dataLoadOperation : NSOperation?
+    @IBOutlet private var cellSelectionOperation : CellSelectionOperation?
+    @IBOutlet private var cellSegueOperation : CellSegueOperation?
+    
+    /// The reuseIdentifier for the cells in the collection view. Must match the reuseIdentifier for the
+    /// Prototype cells in the storyboard
+    @IBInspectable public var reuseIdentifier : String!
+    
+    /// This property is designed to allow other view controllers to pass data in to this view controller
+    public var configurationData : AnyObject? {
+        didSet {
+            if let configureDataOperation = configureDataOperation {
+                configureDataOperation.configurationData = configurationData
+                cellOperationQueue.addOperation(NSBlockOperation(block: { () -> Void in
+                    configureDataOperation.main()
+                    self.refreshDataIfNeeded()
+                }));
+            }
+        }
+    }
+    
+    @IBOutlet private var configureDataOperation : ChocolateConfigureDataOperation?
+    
+    override public func viewDidLoad() {
+        super.viewDidLoad()
+        if let configureDataOperation = configureDataOperation {
+            configureDataOperation.configurationData = configurationData
+            cellOperationQueue.addOperation(NSBlockOperation(block: { () -> Void in
+                configureDataOperation.main()
+            }));
+        }
+        
+        chocolateDataSource = dataSource as? ChocolateCellDataSource
+        
+        if let chocolateDataSource = chocolateDataSource {
+            dataLoadOperation = LoadDataOperation(dataManager: chocolateDataSource)
+            cellOperationQueue.addOperation(dataLoadOperation!)
+        }
+        
+    }
+    
+    override public func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        refreshDataIfNeeded()
+    }
+    
+    private func refreshDataIfNeeded() {
+        if var chocolateDataSource = chocolateDataSource,
+            let tableView = tableView {
+                
+                if chocolateDataSource.needsReload {
+                    dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                        tableView.reloadData()
+                    })
+                    chocolateDataSource.needsReload = false
+                }
+        }
+    }
+    
+    // MARK: - Navigation
+    override public func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if cellSegueOperation == nil {
+            if let selectedChocolateCell = sender as? ChocolateCell {
+                cellSegueOperation = selectedChocolateCell.dynamicType.cellSegueOperation()
+            }
+        }
+        if let cellSegueOperation = cellSegueOperation,
+            selectedCollectionViewCell = sender as? ChocolateCell {
+                cellSegueOperation.cell = selectedCollectionViewCell
+                cellSegueOperation.destinationViewController = segue.destinationViewController
+        }
+        if let cellSegueOperation = cellSegueOperation {
+            let cellSegueBlockOperation = NSBlockOperation(block: { () -> Void in
+                cellSegueOperation.main();
+            })
+            cellOperationQueue.addOperation(cellSegueBlockOperation)
+        }
+    }
+    
+    
+    @IBAction func prepareForUnwind(segue : UIStoryboardSegue) {}
+    
+    // MARK: UITableViewDataSource
+    
+    override public func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        var sectionCount : Int = 0
+        if let chocolateDataSource = chocolateDataSource {
+            let sectionsOp = SectionCountOperation(dataManager: chocolateDataSource){ (number :Int) in
+                sectionCount = number
+            }
+            sectionsOp.addDependency(dataLoadOperation!)
+            cellOperationQueue.addOperation(sectionsOp)
+        }
+
+        cellOperationQueue.waitUntilAllOperationsAreFinished()
+        return sectionCount
+    }
+    
+    
+    override public func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        var itemCount : Int = 0
+        let itemsOp = ItemCountOperation(dataManager: dataSource as! ChocolateCellDataSource, section : section) { (number :Int) in
+            itemCount = number
+        }
+        if let dataLoadOperation = dataLoadOperation {
+            itemsOp.addDependency(dataLoadOperation)
+        }
+        cellOperationQueue.addOperation(itemsOp)
+        cellOperationQueue.waitUntilAllOperationsAreFinished()
+        return itemCount
+    }
+    
+    override public func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier(reuseIdentifier, forIndexPath: indexPath)
+        
+        if let cellClass = cell.dynamicType as? ChocolateCell.Type {
+            let cellConfigOpType = cellClass.cellConfigurationOperation()
+            
+            if let cell = cell as? ChocolateCell,
+                chocolateDataSource = chocolateDataSource {
+                    let cellConfigOp = cellConfigOpType.init(cell: cell, indexPath: indexPath, dataManager: chocolateDataSource)
+                    
+                    cellOperationQueue.addOperation(cellConfigOp);
+            }
+        }
+
+        
+        return cell
+    }
+    
+    // MARK: UITableViewDelegate
+    
+    override public func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        let selectedCell = tableView.cellForRowAtIndexPath(indexPath)
+        
+        if let selectedCell = selectedCell as? ChocolateCell {
+            cellSelectionOperation?.cell = selectedCell
+        }
+        
+        if let cellSelectionOperation = cellSelectionOperation {
+            cellOperationQueue.addOperation(NSBlockOperation(block: { () -> Void in
+                cellSelectionOperation.main()
+            }))
+        }
+        
+    }
+    
+}
